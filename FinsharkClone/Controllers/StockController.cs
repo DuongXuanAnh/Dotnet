4f450ef571ace038313290376d9b1ce24ac67b9c
DuongXuanAnh <thanhniennghiem98@gmail.com>
2025-04-18 15:59:18 +0200

    show API on http://localhost:5162/swagger/index.html

---
+++ FinsharkClone/Controllers/StockController.cs
@@ -0,0 +1,44 @@
+using FinsharkClone.Data;
+using FinsharkClone.Modals;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinsharkClone.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+        
+            var stocks = await _context.Stocks.ToListAsync();
+
+
+            return Ok(stocks);
+        }
+
+         [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+
+        
+    }
+}
--- FinsharkClone/Program.cs
+++ FinsharkClone/Program.cs
@@ -1,7 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using FinsharkClone.Data;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
@@ -10,9 +18,17 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 
 app.Run();
