789087e7efd91dd5b49a90936c4cf68413a441ee
DuongXuanAnh <thanhniennghiem98@gmail.com>
2025-05-11 10:24:53 +0200

    Register

---
+++ FinsharkClone/Dtos/Account/RegisterDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FinsharkClone.Dtos.Account
+{
+    //  dotnet ef migrations add SeedRole   
+    // dotnet ef database update  
+    public class RegisterDto
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+    }
+}
---
+++ FinsharkClone/Migrations/20250511082034_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FinsharkClone.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "37e0d2c1-8b5d-4c1b-bd88-f2c42a405de5", null, "Admin", "ADMIN" },
+                    { "6cbf7629-c500-45fa-9b4b-65c08c335399", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "37e0d2c1-8b5d-4c1b-bd88-f2c42a405de5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6cbf7629-c500-45fa-9b4b-65c08c335399");
+        }
+    }
+}
